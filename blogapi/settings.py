15912fa8c26f30c2836202f2af14416ff728b3ef
Sreerag O S <yolkyoyo@gmail.com>
2025-11-27 19:05:05 +0530

    upload option

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://your-app-media.onrender.com/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 REST_FRAMEWORK = {
